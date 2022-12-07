91ea6c6ded9ba54974157367140729a46ca3c476
duy1111 <maixuanduy0605@gmail.com>
2022-12-07 23:34:39 +0700

    read - CRUD

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -1,33 +1,71 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import './UserManage.scss';
+import { getAllUser } from '../../services/userService';
+import { faPencil, faTrash } from '@fortawesome/free-solid-svg-icons';
 class UserManage extends Component {
-
-    state = {
-
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrUser: [],
+        };
     }
 
-    componentDidMount() {
-
+    async componentDidMount() {
+        let response = await getAllUser('');
+        if (response && response.errCode === 0) {
+            await this.setState({
+                arrUser: response.users,
+            });
+            console.log('data get all user', this.state.arrUser);
+        }
     }
-
 
     render() {
+        let arrUsers = this.state.arrUser;
+        console.log('check render', arrUsers);
         return (
-            <div className="text-center">Manage users</div>
+            <div className="user-container">
+                <div className="title text-center">Manage user with Duy</div>
+                <div className="user-table mt-4 mx-3">
+                    <table id="customers">
+                        <tr>
+                            <th>Email</th>
+                            <th>FirstName</th>
+                            <th>LastName</th>
+                            <th>Address</th>
+                            <th>Action</th>
+                        </tr>
+                        {arrUsers &&
+                            arrUsers.map((item, index) => {
+                                return (
+                                    <tr>
+                                        <td>{item.email}</td>
+                                        <td>{item.firstName}</td>
+                                        <td>{item.LastName}</td>
+                                        <td>{item.address}</td>
+                                        <td>
+                                            <button className='btn-edit' ><FontAwesomeIcon icon={faPencil} /></button>
+                                            <button className='btn-delete' ><FontAwesomeIcon icon={faTrash} /></button>
+                                        </td>
+                                    </tr>
+                                );
+                            })}
+                    </table>
+                </div>
+            </div>
         );
     }
-
 }
 
-const mapStateToProps = state => {
-    return {
-    };
+const mapStateToProps = (state) => {
+    return {};
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-    };
+const mapDispatchToProps = (dispatch) => {
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
--- src/services/userService.js
+++ src/services/userService.js
@@ -2,5 +2,8 @@ import axios from '../axios';
 const handleLogin = (email, password) => {
     return axios.post('/api/login', { email:email, password:password });
 };
-
+const getAllUser = (userId) => {
+    return axios.get(`/api/get-all-user?id=${userId}`,{id:userId})
+}
 export default handleLogin;
+export {getAllUser}
